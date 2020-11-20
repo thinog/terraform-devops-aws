5b7b2c0ba9068147fa76919fd2e6c7d1e7997b97
Thiago Martins <thinomar@gmail.com>
2020-11-20 19:38:24 -0300

    Criação de VPN, SG, IGW, Subnets e Route Tables de aplicação escalável

---
+++ 03-auto-scaling-application/main.tf
@@ -0,0 +1,185 @@
+provider "aws" {
+  region = var.region
+}
+
+locals {
+  tags = {
+    Name = "Terraform"
+  }
+}
+
+resource "aws_vpc" "vpc" {
+  cidr_block = "192.168.0.0/16"
+  tags       = local.tags
+}
+
+resource "aws_internet_gateway" "gw" {
+  depends_on = [aws_vpc.vpc]
+  vpc_id     = aws_vpc.vpc.id
+  tags       = local.tags
+}
+
+resource "aws_subnet" "public_a" {
+  depends_on        = [aws_vpc.vpc]
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "192.168.1.0/24"
+  availability_zone = "${var.region}a"
+  tags = {
+    Name = "Terraform public 1a"
+  }
+}
+
+resource "aws_subnet" "public_b" {
+  depends_on        = [aws_vpc.vpc]
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "192.168.2.0/24"
+  availability_zone = "${var.region}b"
+  tags = {
+    Name = "Terraform public 1b"
+  }
+}
+
+resource "aws_subnet" "private_a" {
+  depends_on        = [aws_vpc.vpc]
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "192.168.3.0/24"
+  availability_zone = "${var.region}a"
+  tags = {
+    Name = "Terraform private 1a"
+  }
+}
+
+resource "aws_subnet" "private_b" {
+  depends_on        = [aws_vpc.vpc]
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "192.168.4.0/24"
+  availability_zone = "${var.region}b"
+  tags = {
+    Name = "Terraform private 1b"
+  }
+}
+
+resource "aws_route_table" "rt_public" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "Terraform Public"
+  }
+}
+
+resource "aws_route_table" "rt_private" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "Terraform Private"
+  }
+}
+
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.rt_public.id
+}
+
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.rt_public.id
+}
+
+resource "aws_route_table_association" "private_a" {
+  subnet_id      = aws_subnet.private_a.id
+  route_table_id = aws_route_table.rt_private.id
+}
+
+resource "aws_route_table_association" "private_b" {
+  subnet_id      = aws_subnet.private_b.id
+  route_table_id = aws_route_table.rt_private.id
+}
+
+resource "aws_security_group" "web" {
+  name        = "web"
+  description = "Allow public inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port       = -1
+    to_port         = -1
+    protocol        = "icmp"
+    cidr_blocks = [aws_vpc.vpc.cidr_block]
+  }
+
+  egress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = [aws_subnet.private_a.cidr_block]
+  }
+
+  tags = {
+    Name = "Terraform Web"
+  }
+}
+
+resource "aws_security_group" "db" {
+  depends_on = [ aws_security_group.web ]
+  name        = "db"
+  description = "Allow incoming database connections"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.web.id]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.vpc.cidr_block]
+  }
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = [aws_vpc.vpc.cidr_block]
+  }
+
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Terraform Database"
+  }
+}
